975f899a5f98491b6eba432746aed79e4dc960c0
jiktor <viktorjgeorgiev@gmail.com>
2023-12-08 11:31:27 +0200

    added form for registration without functionality

--- websiteFontend/src/app/register/register.component.ts
+++ websiteFontend/src/app/register/register.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  title ='Registartion form'
+  reactiveForm: FormGroup;
 
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      firstName: new FormControl(null,[this.noSpaceAllowed,Validators.required]),
+      lastName: new FormControl(null,[this.noSpaceAllowed,Validators.required]),
+      email : new FormControl(null, [Validators.email,Validators.required]),
+      password: new FormControl(null,Validators.required),
+      confirmPassword: new FormControl(null,Validators.required)
+    });
+  }
+
+  onSubmit(){
+    console.log(this.reactiveForm)
+  }
+  //custom validation for whitespace
+  noSpaceAllowed(control: FormControl){
+    if(control.value != null && control.value.indexOf(' ') != -1){
+      return {noSpaceAllowed: true}
+    }
+    return null;
+  }
+  //custom validation for matching passwords
+  // matchingPassword(control: FormControl){
+  //   //console.log(control.value)
+  //  // console.log((<FormGroup>this.reactiveForm).get('password').value)
+  //   if(!control.value != (<FormControl>this.reactiveForm.get('password')).value){
+  //     return {notMatchingPassword: true}
+  //   }
+  //   return null;
+  // }
 }
