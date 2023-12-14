cadd165435f300475f41482323d3b65f32ec3e39
jiktor <viktorjgeorgiev@gmail.com>
2023-12-14 15:09:42 +0200

    added sql script to insert test data on project initialisation

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO brands_dao
+  VALUES
+  (1, 'BMW'),
+  (2, 'MERCEDES'),
+  (3, 'AUDI'),
+  (4, 'SKODA'),
+  (5, 'RENAULT'),
+  (6, 'CITROEN');
+INSERT INTO models_dao
+  VALUES
+  (1,2, 'X4'),
+  (1,1, 'X3'),
+  (1,3, 'X5'),
+  (2,8, 'CLA'),
+  (2,9, 'CLG'),
+  (2,10, 'GLA'),
+  (2,11, 'GLC'),
+  (3,4, 'A3'),
+  (3,5, 'A4'),
+  (3,6, 'A5'),
+  (3,7, 'A6');
