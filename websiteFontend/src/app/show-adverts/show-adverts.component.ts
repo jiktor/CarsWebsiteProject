ac13384ee7df645cc51b2b1b048660f403284888
jiktor <viktorjgeorgiev@gmail.com>
2024-03-02 14:32:00 +0200

    added visualisation for all adverts

--- websiteFontend/src/app/show-adverts/show-adverts.component.ts
+++ websiteFontend/src/app/show-adverts/show-adverts.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { CreateAdvertModel } from '../Models/createAdvertModel';
 import { CreateAdvertService } from '../Services/advertsService.service';
 import { Subscription } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-show-adverts',
   standalone: true,
   providers: [CreateAdvertService],
-  imports: [],
+  imports: [NgFor],
   templateUrl: './show-adverts.component.html',
   styleUrl: './show-adverts.component.css'
 })
 export class ShowAdvertsComponent {
+  divState = 'normal';
+
+  ngOnInit(): void {
+  }
+
+  onAdvertClick(_t6: any) {
+  throw new Error('Method not implemented.');
+  }
 
   adverts : CreateAdvertModel[];
   subscription: Subscription;
@@ -19,20 +29,26 @@ export class ShowAdvertsComponent {
   constructor( private createAdvertService: CreateAdvertService){}
 
   async ngAfterViewInit(){
-    console.log("from ng on in it")
     await this.getAllAdverts();
   }
-
-  showAds(){
-    this.adverts.forEach(element => {
-      console.log(element)
-    });
-  }
-
   async getAllAdverts(){
     this.subscription = this.createAdvertService.getAllAdverts().subscribe((data:CreateAdvertModel[])=>{
       this.adverts = data
     });
   }
+
+  getImageUrl(blob: Blob): string {
+    if (blob instanceof Blob) {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = () => {
+            return reader.result as string;
+        };
+    } else {
+        console.error('Parameter is not a Blob:', blob);
+        return ''; // or any default value
+    }
+    return "";
+}
 
 }
